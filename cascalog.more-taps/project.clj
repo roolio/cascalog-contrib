c63d69e8a414c24987ec14ddf5e29e06479d3a56
roolio <jlaugel@gmail.com>
2012-10-05 19:24:53 +0300

    modif version protobuf

--- cascalog.more-taps/project.clj
+++ cascalog.more-taps/project.clj
@@ -1,7 +1,7 @@
 (defproject cascalog-more-taps "0.2.1"
   :description "Taps for Cascalog"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascalog "1.9.0"]
-                 [org.pingles/cascading.protobuf "0.0.1"]]
+                 [cascalog "1.10.0"]
+                 [org.pingles/cascading.protobuf "0.0.3"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
   :repositories {"conjars" "http://conjars.org/repo"})
